3b5fe1b261dfd2f8358b133d82887bcec9f2b7f8
Luca <luca@lambia.it>
2017-12-22 03:36:02 +0100

    KISS
    Keep It Simple, Stupid.

--- load-stuff.js
+++ load-stuff.js
@@ -1,6 +1,13 @@
-// You can bookmarklet this, like: javascript:blablabla;
+// You can bookmarklet this
 
-var jq = document.createElement('script');
-jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js";
-document.getElementsByTagName('head')[0].appendChild(jq);
+javascript:void((function(){
+	var version = "3.2.1";
+	var jq = document.createElement('script');
+	jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/"+version+"/jquery.min.js";
+	document.getElementsByTagName('head')[0].appendChild(jq);
+	console.log("jQuery "+version+" Loaded");
+})());
+
+//Just for reminder
 //jQuery.noConflict();
+//<meta http-equiv="Content-Security-Policy" content="default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'" />
--- stuff.js
+++ stuff.js
@@ -1,75 +1,116 @@
 /*
-var jq = document.createElement('script');
-jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js";
-document.getElementsByTagName('head')[0].appendChild(jq);
+Whatsapp Stealth Logger v. 0.2
+by Luca Lambia, 2017
+
+For educational purpose only.
+Code intended to be copy-n-pasted into Chrome Inspector Console (F12)
 */
 
-var counter = 0;
-var author;
-var time;
-var message;
-var avatar;
+//load-stuff.js
+var version = "3.2.1";
+var jq = document.createElement('script');
+jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/"+version+"/jquery.min.js";
+document.getElementsByTagName('head')[0].appendChild(jq);
+console.log("jQuery "+version+" Loaded");
 
-// 1. merge text and emoji in author and message (remove text, parse)
-// 2. avatar doesn't work
-// 3. note that this expose to trouble if there more than one person with the same name
-// 4. use regex less strict (only spaces?) (/\W/g) > (/[^A-Za-z0-9]/g) > (/[^A-Za-z0-9]\/_?:.,/g) > (/[^-A-Za-z0-9]/g) 
-// 5. if group message, include: span.chat-status-name and span.chat-status-divider
-// 6. use rando to trigger with random sender
-// 7. centrare a $("header.pane-header.pane-list-header").position()
+//Create our "space"
+var style = `<style>
+body {
+	color: white; background-color: #222;
+	font-family: Verdana, sans-serif;
+}
+span {
+	display: block;
+}
+.users {
+	padding: 0.5em;
+}
+</style>`;
 
-// X Button + Overlay icon
-// Make as Chrome Extension
+var win = window.open("", "Whatsapp Stealth", "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,fullscreen=yes,width="+screen.width+",height="+screen.height);
+win.document.body.innerHTML = style;
 
-//Notification trigger
-function triggera(primo) {
-    //var rando = Math.floor((Math.random() * $(".infinite-list-item.infinite-list-item-transition").length-1) + 0); //6
-    if (primo) {
-        $(".first.infinite-list-item.infinite-list-item-transition").find(".chat-status").children("span").html("<!-- commento -->"+Date.now());
-    } else {
-        $(".infinite-list-item.infinite-list-item-transition").last().find(".chat-status").children("span").html("<!-- commento -->"+Date.now());
-    }
+//Understand where the stuff is
+var leftPanel = {class: ".chatlist-panel-body", id: "#pane-side" };
+var chatRows = {class: ".chat" };
+var thisChat = {class: ".active" };
+
+//"Globals"
+var buffer = {};
+var thetime = null;
+
+//Copy the last message of the first chats to an array with keys (I know, contact's names are bad)
+function init() {
+	$(".chatlist-panel-body").scrollTop(0);
+
+	$( "#pane-side .chat .chat-body .chat-secondary .chat-status span .ellipsify" ).each(function( index ) {
+		who = $(this).parent().parent().parent().parent().find(".chat-main .chat-title span").text();
+		what = $(this).text();
+		//pic = $(this).parent().parent().parent().parent().parent().find(".chat-avatar .avatar span > img").attr("src").toString();
+
+		buffer[who] = [what];
+	});
+
+	thetime = window.setTimeout(check, 100);
 }
 
-$("body").append("<div id='injectedBox' style='display: none; position: fixed; top: 0px; left: 0px; width: 100%; height: 100%; background-color: rgba(30, 190, 165,0.92); overflow-y: scroll; padding: 1em; box-sizing: border-box;'>"+
-                 "<div id='injectedClose' class='injectedUI' style='position: fixed; cursor: pointer; top: 0.25em; right: 0.25em; font-size: 1.5em;'>&#10006;</div>"+
-                 "<span style='font-style: italic; font-weight: bold;'>Welcome to Whatsapp Stealth Logger 0.1<br/>Notifications will be shown below.</span>"+
-                 "</div>"+
-                 "<div id='injectedPop' class='injectedUI' style='position: fixed; top: 0; right: 50%; font-size: 0.8em; color: white; background-color: rgba(30, 190, 165,0.92); padding: 0.45em; cursor: pointer;'>Stealth</div>");
+//Same as init, but check if already buffered or new message
+function check() {
+	$(".chatlist-panel-body").scrollTop(0);
 
-$("#injectedPop").css({"margin-right": -$("#injectedPop").width()/2}); //7
-$("#injectedClose").click(function() {
-    $("#injectedBox").fadeOut(350);
-    $("#injectedPop").fadeIn(350);
-});
-$("#injectedPop").click(function() {
-    $("#injectedBox").fadeIn(350);
-    $("#injectedPop").fadeOut(350);
-});
+	$( "#pane-side .chat .chat-body .chat-secondary .chat-status span .ellipsify" ).each(function( index ) {
+		who = $(this).parent().parent().parent().parent().find(".chat-main .chat-title span").text();
+		what = $(this).text();
+		//pic = $(this).parent().parent().parent().parent().parent().find(".chat-avatar .avatar span > img").attr("src").toString();
 
-$(".chat-status").on('DOMSubtreeModified', "span", function () {
-    author = $(this).parent().parent().parent().find(".chat-title").find("span").contents().filter(function(){ return this.nodeType != 8; }).text(); //1
-    
-    time = $(this).parent().parent().parent().find(".chat-meta").find("span.chat-time").contents().filter(function(){ return this.nodeType != (1 || 8); }).text(); //1
-    
-    message = $(this).text(); //5
-    //.find("span.emojitext").contents().filter(function(){ return this.nodeType != 8; }).text();
-    
-    //avatar =  $(this).parent().parent().parent().find(".avatar-body").find("img"); //.attr('src'); //2
-    
-    //var author = "Arianna"; var time = "03:21"; var message = "Ciao amorino!";
-    
-    if(!$("#injectedAuthor-"+author.replace(/\W/g, '')).length) // 3 //4
-    {
-        $("<div id='injectedAuthor-"+author.replace(/\W/g, '')+"' style='margin-top: 1em;'>"+ //4
-         "<span class='author' style='float:left; font-weight: bold;'>"+author+"</span>"+
-         "<span class='time' style='float:right;'>"+time+"</span>"+
-         "<span class='message' style='clear: both; display: block;'>"+message+"</span>"+
-         "</div>").appendTo("#injectedBox");
-    
-    } else {
-        $("<span class='message' style='clear: both; display: block;'>"+message+"</span>").appendTo("#injectedAuthor-"+author);
-        $("#injectedAuthor-"+author).find(".time").text(time);
-    }
+		//If we are checking a message from a known contact..
+		if( buffer[who] ) {
+			var c = buffer[who].length-1;
+			//...and its text is not buffered
+			if( buffer[who][c].toString() != what.toString() ) {
+				buffer[who].push(what);
+				writedown(who,what);
+			} else {
+				//else buffer is still valid
+			}
+		} else {
+			//If we are checking a message from a new contact, add everything to the array
+			buffer[who] = [what];
+			writedown(who,what)
+		}
+	});
+	thetime = window.setTimeout(check, 100);
+}
 
-});
+//Close it
+function killer() {
+	clearInterval(thetime);
+	$("#logger").empty().remove();
+}
+
+//Write a new div to our frontend
+function writenew(who,what) {
+	win.document.body.innerHTML += "<div id='"+who+"'><span><b>"+who+"</b></span></div>";
+}
+
+//Write a new content to our frontend
+function writedown(who,what) {
+	if( !win.document.getElementById(who) ) {
+		writenew(who,what);
+	}
+	win.document.getElementById(who).innerHTML += "<span>"+what+"</span>";
+}
+
+init();
+
+/*
+ToDo:
+bootstrap layout
+use avatar img
+save complete conversations each time one become opened
+handle emoticon and audio recordings
+save everything to file
+use a DOMChanged, MutationObserver or similar function instead of timers, like in the first version, but working with React
+put everything in a chrome extension or apk?
+oneliner function (ajax) that download the script, css and layout
+*/
